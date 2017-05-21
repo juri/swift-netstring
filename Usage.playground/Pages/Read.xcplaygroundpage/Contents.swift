7e60af35caa64aa4ffc34355f88f5c68ed831326
Juri Pakaste <juri@iki.fi>
2017-05-21 10:09:06 +0300

    Add example with file reader

--- Usage.playground/Pages/Read.xcplaygroundpage/Contents.swift
+++ Usage.playground/Pages/Read.xcplaygroundpage/Contents.swift
@@ -6,3 +6,21 @@ let data: [UInt8] = [0x34, 0x3a, 0x73, 0x77, 0x61, 0x67, 0x2c]
 if let ns = Netstring(array: data) {
     let payload = ns.payload
 }
+
+// Read data from a file (see Usage.playground/Resources)
+let path = Bundle.main.path(forResource: "Data", ofType: "txt")!
+let fh = FileHandle(forReadingAtPath: path)!
+
+func fileReader(handle: FileHandle) -> Netstring.Reader {
+    return { (length: Int) -> [UInt8] in
+        let data = handle.readData(ofLength: length)
+        let arr = [UInt8](data)
+        return arr
+    }
+}
+
+if let ns2 = Netstring(reader: fileReader(handle: fh)),
+   let json = try? JSONSerialization.jsonObject(with: Data(bytes: ns2.payload), options: [])
+{
+    let dict = json as! [String: String]
+}
